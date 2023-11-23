455a89d81d6161d9869e5b9b83488365fc03e5a1
Florian Böhm <f.boehm@tu-braunschweig.de>
2023-11-23 16:13:46 +0100

    CFG: handle IF control structure

--- src/main/scala/io/shiftleft/js2cpg/passes/CfgCreationPass.scala
+++ src/main/scala/io/shiftleft/js2cpg/passes/CfgCreationPass.scala
@@ -1,8 +1,8 @@
 package io.shiftleft.js2cpg.passes
 
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.codepropertygraph.generated.EdgeTypes
-import io.shiftleft.codepropertygraph.generated.nodes.{AstNode, Block, Call, Local, Method, MethodReturn}
+import io.shiftleft.codepropertygraph.generated.{ControlStructureTypes, EdgeTypes}
+import io.shiftleft.codepropertygraph.generated.nodes.{AstNode, Block, Call, ControlStructure, Local, Method, MethodReturn}
 import io.shiftleft.js2cpg.core.Report
 import io.shiftleft.js2cpg.utils.SourceWrapper.*
 import io.shiftleft.passes.ConcurrentWriterCpgPass
@@ -12,7 +12,10 @@ import org.slf4j.LoggerFactory
 import scala.collection.mutable.ListBuffer
 import scala.util.{Failure, Success, Try}
 
-
+/**
+ * Given a populated CPG, this pass creates the control flow graph (CFG) for each method.
+ * Methods are processed in parallel.
+ */
 class CfgCreationPass(cpg: Cpg, report: Report)
     extends ConcurrentWriterCpgPass[Method](cpg) {
 
@@ -49,24 +52,73 @@ class CfgCreationPass(cpg: Cpg, report: Report)
       case _: Method | _: Local =>
         // Ignore.
         lastNodes
-      case _: Call =>
+      case astNode: Call =>
         handleCfgCall(astNode, lastNodes)
+      case astNode: ControlStructure =>
+        handleCfgControlStructure(astNode, lastNodes)
       case _ =>
         handleCfgGeneric(astNode, lastNodes)
     }
   }
 
-  private def handleCfgCall(astNode: AstNode, lastNodes: List[AstNode])
+  private def handleCfgControlStructure(controlStructure: ControlStructure, lastNodes: List[AstNode])
+                                       (implicit diffGraph: DiffGraphBuilder): List[AstNode] = {
+    controlStructure.controlStructureType match {
+      case ControlStructureTypes.IF =>
+        handleCfgControlStructureIf(controlStructure, lastNodes)
+      case _ =>
+        logger.warn(s"unhandled control structure type '${controlStructure.controlStructureType}'.")
+        lastNodes
+    }
+  }
+
+  private def handleCfgControlStructureIf(controlStructure: ControlStructure, lastNodes: List[AstNode])
+                                         (implicit diffGraph: DiffGraphBuilder): List[AstNode] = {
+    // Link condition node to lastNodes
+    val conditionNodeList: List[AstNode] = createConditionNodeList(controlStructure, lastNodes)
+    // Now add if and else Nodes and connect them to the condition node
+    val ifNodeList: List[AstNode] = createTrueNodeList(controlStructure, conditionNodeList)
+    val elseNodeList: List[AstNode] = createFalseNodeList(controlStructure, conditionNodeList)
+    (new ListBuffer[AstNode] ++= ifNodeList ++= elseNodeList).toList
+  }
+
+  private def createConditionNodeList(controlStructure: ControlStructure, lastNodes: List[AstNode])
+                                     (implicit diffGraph: DiffGraphBuilder): List[AstNode] = {
+    // Link condition node to lastNodes
+    createCfgStep(controlStructure.astChildren.filter(_.order == 1).head, lastNodes)
+  }
+
+  private def createTrueNodeList(controlStructure: ControlStructure, conditionNodeList: List[AstNode])
+                                (implicit diffGraph: DiffGraphBuilder): List[AstNode] = {
+    var trueNodeList: List[AstNode] = conditionNodeList
+    if (controlStructure.astChildren.exists(_.order == 2)) {
+      // TRUE Edges
+      trueNodeList = createCfgStep(controlStructure.astChildren.filter(_.order == 2).head, conditionNodeList)
+    }
+    trueNodeList
+  }
+
+  private def createFalseNodeList(controlStructure: ControlStructure, conditionNodeList: List[AstNode])
+                                 (implicit diffGraph: DiffGraphBuilder): List[AstNode] = {
+    var falseNodeList: List[AstNode] = conditionNodeList
+    if (controlStructure.astChildren.exists(_.order == 3)) {
+      // FALSE Edges
+      falseNodeList = createCfgStep(controlStructure.astChildren.filter(_.order == 3).head, conditionNodeList)
+    }
+    falseNodeList
+  }
+
+  private def handleCfgCall(call: Call, lastNodes: List[AstNode])
                               (implicit diffGraph: DiffGraphBuilder): List[AstNode] = {
     var localLastNodes: List[AstNode] = lastNodes
-    if (astNode.astChildren.toArray.isEmpty) {
-      localLastNodes = handleCfgNoChildren(astNode, localLastNodes)
+    if (call.astChildren.toArray.isEmpty) {
+      localLastNodes = handleCfgNoChildren(call, localLastNodes)
     } else {
       // Catch special calls here
-      localLastNodes = handleCfgGeneric(astNode, lastNodes)
+      localLastNodes = handleCfgGeneric(call, lastNodes)
     }
-    addEdgesForAllLastNodes(astNode, localLastNodes)
-    new ListBuffer[AstNode].addOne(astNode).toList
+    addEdgesForAllLastNodes(call, localLastNodes)
+    new ListBuffer[AstNode].addOne(call).toList
   }
 
   private def handleCfgGeneric(astNode: AstNode, lastNodes: List[AstNode])
